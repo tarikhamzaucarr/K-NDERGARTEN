6ab5302bea1fe9f3ba333918ef7cded3415625fa
tarikhamzaucarr <review.page29@gmail.com>
2026-04-01 17:38:18 +0300

    Delete kindergarten-photo-studio 3 directory

--- kindergarten-photo-studio 3/components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-type ContainerProps = PropsWithChildren<{
-  className?: string;
-}>;
-
-export function Container({ children, className = '' }: ContainerProps) {
-  return <div className={`container ${className}`.trim()}>{children}</div>;
-}
